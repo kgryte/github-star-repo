fe613582bed305327f2b482701e99f2cf666acf2
kgryte <kgryte@gmail.com>
2016-03-04 04:06:28 -0800

    Add request tests and add test fixtures

---
+++ test/fixtures/opts.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function getOpts() {
+	var opts = {
+		'token': 'abcdef',
+		'hostname': 'api.github.com',
+		'port': 443,
+		'protocol': 'https'
+	};
+	return opts;
+}
+
+
+// EXPORTS //
+
+module.exports = getOpts;
